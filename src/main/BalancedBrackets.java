de3d4e709b29cfe273edbdc02fb167a177ff937e
Prashanthi Chirunomula <chirunomulap@gmail.com>
2020-12-15 10:14:46 -0600

    Unit Testing Studio

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,35 +1,42 @@
 package main;
 
 
+import java.util.ArrayList;
+
 public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
-     *
+     * <p>
      * That is, whether it consists entirely of pairs of opening/closing
      * brackets (in that order), none of which mis-nest. We consider a bracket
      * to be square-brackets: [ or ].
-     *
+     * <p>
      * The string may contain non-bracket characters as well.
-     *
+     * <p>
      * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
+     * "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     * <p>
      * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     * "[LaunchCode", "Launch]Code[", "[", "]["
      *
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
+
+
     public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
+        ArrayList<Character> brackets = new ArrayList<>();
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
-                brackets++;
+                brackets.add(ch);
             } else if (ch == ']') {
-                brackets--;
+                if (brackets.isEmpty()) {
+                    return false;
+                }
+                brackets.remove(0);
             }
         }
-        return brackets == 0;
+        return brackets.isEmpty();
     }
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,75 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void stringInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Unit Testing]"));
+    }
+
+    @Test
+    public void stringInsideAndOutsideBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Unit] Testing"));
+    }
+
+    @Test
+    public void anEmptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void aStringContainEmptyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Unit Testing"));
+    }
+
+    @Test
+    public void aStringWithMultipleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Testing:[Manual,Automation]"));
+    }
+
+    @Test
+    public void bracketsInsideBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Test1], Test2]"));
+    }
+
+    @Test
+    public void aStringWithNoClosingBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[Test"));
+    }
+
+    @Test
+    public void anInvalidOpenBracketsInStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Test["));
+    }
+
+    @Test
+    public void anInvalidClosingBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]T["));
+    }
+
+    @Test
+    public void multipleBracketsNoProperClosingReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[Test]"));
+    }
+
+    @Test
+    public void specialCharactersInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[@&]"));
+    }
+
+    @Test
+    public void multipleBracketsButNotProperlyArrangedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]T1]]T2[[T3[[T4"));
+    }
 
 }
